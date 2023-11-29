399c3d7f2c30b298c6af7f7de1c6e94f101f274d
Jun <jun@Allisons-Mac-mini.local>
2023-11-29 14:08:34 -0500

    typo fix. sample code added for translation.

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,8 +1,9 @@
 # pip install geopy
+# pip install googletrans==3.1.0a0
 
 import pandas as pd
 from geopy.geocoders import Nominatim
-import time
+from googletrans import Translator
 
 geo_local = Nominatim(user_agent='South Korea')
 
@@ -12,7 +13,15 @@ def geocoding(address):
     
 if __name__ == "__main__":
     df = pd.read_csv("ev_charging_stations.csv", encoding="cp949")
-    df.to_csv('ev_charging_stations_parsed.csv', index=False)
+    #df.to_csv('ev_charging_stations_parsed.csv', index=False)
 
     df1 = pd.read_csv("gas_stations_seoul.csv", encoding="cp949")
-    df1.to_csv('gas_stations_seoul_parsed.csv', index=False)
+    #df1.to_csv('gas_stations_seoul_parsed.csv', index=False)
+    translator = Translator()
+
+    #TODO: translate only the columns we need to reduce running time
+    for column in df1.columns:
+        print(column)
+        df1[column] = df1[column].apply(translator.translate, src='ko', dest='en').apply(getattr, args=('text',))
+
+    print(df1.head())
